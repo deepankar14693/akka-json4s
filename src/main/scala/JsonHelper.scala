515ba31c0d0ed547ef33bafcead9bd3654917936
deepankar <deepankar.ranswal@knoldus.in>
2018-06-05 10:54:16 +0530

    removed comments from files

--- src/main/scala/JsonHelper.scala
+++ src/main/scala/JsonHelper.scala
@@ -12,29 +12,9 @@ trait JsonHelper extends {
 
   implicit val formats: Formats = DefaultFormats
 
-  /*
-takes any type of value and serializes it to string
-eg: val student = Student(1,"deepankar")
-     write(student) will serialize it to {"rollNum":1,"name":"deepankar"}
- */
   def write[T <: AnyRef](value: T): String = jWrite(value)
 
-  /*
-  Any valid json can be parsed into internal AST(Abstract Syntax Tree) format.
-  eg : If this is our json string coming from a post request
-       val jsonString = {"rollNum":1,"name":"deepankar"}
-       parse(jsonString) will parse it to something like
-       JObject(List((rollNum,JInt(1)), (name,JString(deepankar))))
-   */
   protected def parse(value: String): JValue = jParser(value)
-
-  /*
-  extract method is used after parsing the json string, as parse method returns JValue so we
-  can map this JValue to our target scala data model
-  eg :  val jsonString = {"rollNum":1,"name":"deepankar"}
-       val parseString = parse(jsonString)
-       extract(parseString) will return Student(?1,"deepankar")
-   */
 
   implicit protected def extractOrEmptyString(json: JValue): String = {
     json match {
--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -3,19 +3,14 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-
 import scala.concurrent.ExecutionContextExecutor
-import scala.util.control.NonFatal
 
-//the usual config code required for an Akka Http server, mixing in our JsonHelper trait too
 object Routes extends App with JsonHelper with Students {
 
   implicit val system: ActorSystem = ActorSystem("my-system")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
-  // needed for the future flatMap/onComplete in the end
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
   case class Error(message: String)
-//  val studentsClass = new Students
 
  lazy val route =
     pathPrefix("demo") {
@@ -25,9 +20,6 @@ object Routes extends App with JsonHelper with Students {
             val addedStudents = fetchStudents
             val jsonResponse = write(addedStudents)
 
-            /* json for list of Students
-            [{"rollNum":1,"name":"Ayush"},{"rollNum":2,"name":"deepankar"}]
-             */
             HttpResponse(entity = HttpEntity(ContentTypes.`application/json`, jsonResponse))
           }
         }
@@ -37,10 +29,6 @@ object Routes extends App with JsonHelper with Students {
             entity(as[String]) { studentJson => {
               complete {
                 val studentRecord = parse(studentJson).extract[Student]
-
-                /*
-                Student(3,randhir)
-                 */
                 val updatedList = addStudent(studentRecord)
                 val jsonResponse = write(updatedList)
                 HttpResponse(entity = HttpEntity(ContentTypes.`application/json`, jsonResponse))
@@ -50,8 +38,6 @@ object Routes extends App with JsonHelper with Students {
           }
         } ~
         put {
-
-          //for updating student name student roll number has to be same as entered previously
           path("updateStudent") {
             entity(as[String]) { studentJson => {
               complete {
